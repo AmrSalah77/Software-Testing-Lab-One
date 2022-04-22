29743659acd50f864b4167549c1587ccc25124b7
Amr Salah <amrs22131@gmail.com>
2022-04-22 17:17:28 +0200

    added test cases

--- Test/lab1Test.java
+++ Test/lab1Test.java
@@ -4,29 +4,41 @@ import static org.junit.Assert.*;
 
 public class lab1Test {
 
-    //branch coverage
     @Test
     public void testweight() {
         Weight tester = new Weight();
-        assertEquals(false,tester.weightcheck(2));
-        assertEquals(true,tester.weightcheck(4));
+        assertFalse(tester.weightcheck(2));
+    }
+
+    @Test
+    public void testweight1() {
+        Weight tester = new Weight();
+        assertTrue(tester.weightcheck(50));
+    }
+
+    @Test
+    public void testweight2() {
+        Weight tester = new Weight();
+        assertTrue(tester.weightcheck(4));
+    }
+
+    @Test
+    public void testweight3() {
+        Weight tester = new Weight();
+        assertFalse(tester.weightcheck(1));
     }
 
 
-    //testing boundaries
     @Test(expected = IllegalArgumentException.class)
             public void testExceptionIsThrown(){
         Weight tester = new Weight();
         tester.weightcheck(0);
-        tester.weightcheck(101);
     }
-    @Test
-    public void testweightcheck() {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testExceptionIsThrown1(){
         Weight tester = new Weight();
-        assertEquals(false,tester.weightcheck(1));
-        assertEquals(false,tester.weightcheck(2));
-        assertEquals(false,tester.weightcheck(99));
-        assertEquals(true,tester.weightcheck(100));
+        tester.weightcheck(101);
     }
 
 }
