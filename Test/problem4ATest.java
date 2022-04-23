0a44ede7384b8fdebc473d6660e60677c6cb445a
Amr Salah <amrs22131@gmail.com>
2022-04-23 11:27:24 +0200

    renamed

--- Test/problem4ATest.java
+++ Test/problem4ATest.java
@@ -10,29 +10,47 @@ public class lab1Test {
         assertFalse(tester.weightcheck(2));
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testExceptionIsThrown(){
+        Weight tester = new Weight();
+        tester.weightcheck(0);
+    }
+
     @Test
     public void testweight1() {
         Weight tester = new Weight();
-        assertTrue(tester.weightcheck(50));
+        assertFalse(tester.weightcheck(1));
     }
 
     @Test
     public void testweight2() {
         Weight tester = new Weight();
-        assertTrue(tester.weightcheck(4));
+        assertTrue(tester.weightcheck(8));
     }
 
     @Test
     public void testweight3() {
         Weight tester = new Weight();
-        assertFalse(tester.weightcheck(1));
+        assertTrue(tester.weightcheck(50));
     }
 
 
-    @Test(expected = IllegalArgumentException.class)
-            public void testExceptionIsThrown(){
+    @Test
+    public void testweight4() {
         Weight tester = new Weight();
-        tester.weightcheck(0);
+        assertFalse(tester.weightcheck(67));
+    }
+
+    @Test
+    public void testweight5() {
+        Weight tester = new Weight();
+        assertFalse(tester.weightcheck(99));
+    }
+
+    @Test
+    public void testweight6() {
+        Weight tester = new Weight();
+        assertTrue(tester.weightcheck(100));
     }
 
     @Test(expected = IllegalArgumentException.class)
